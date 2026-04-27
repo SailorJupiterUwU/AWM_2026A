2816ae5a3538aad0cc40f926477d4627b474e061
SailorJupiterUwU <anaelapozo192@gmail.com>
2026-04-27 15:51:07 -0500

    Prueba

--- 20-04-2026/script.js
+++ 20-04-2026/script.js
@@ -4,13 +4,26 @@
     titulo.style.backgroundColor = "blue";
     titulo.innerText = "Hola"
 }*/
+//var estudiante={id:1, nombre:"Danny Guaman"};
+const lstestudiantes=[
+    {id:1, nombre:"pepe"},
+    {id:2, nombre:"Cristina"},
+    {id:2, nombre:"Daniel"}
+];
+
+/*lstestudiantes.forEach((estudiante)=>{
+    console.log(estudiante);
+});*/
 
 //Agregar estudiante
-const insertarEstudiante = () => {
+const renderizarListaEstudiantes = () => {
     const divContendeor = document.getElementById("contenedor");
-    const itemEstudiante = document.createElement("p");
-    itemEstudiante.innerText = "OwO";
-    divContendeor.appendChild(itemEstudiante);
+    divContendeor.innerText="";
+    lstestudiantes.forEach((estudiante)=>{
+        const itemEstudiante = document.createElement("p");
+        itemEstudiante.innerText = estudiante.nombre;
+        divContendeor.appendChild(itemEstudiante);
+    });
 }
 
 //cambiar color
@@ -18,4 +31,10 @@ const insertarEstudiante = () => {
 /*function hola(){
     return console.log("Hola");
 }*/
+const insertarEstudiante = (event) =>{
 
+    event.preventDefault();
+    const nombreIngresado = document.getElementById("txtNombre").value;
+    lstestudiantes.push({id:0, nombre: nombreIngresado});
+    renderizarListaEstudiantes();
+}
