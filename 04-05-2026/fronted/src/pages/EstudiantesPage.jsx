ca386492b02aad4580cce6f889779f08cd3e56b6
SailorJupiterUwU <anaelapozo192@gmail.com>
2026-05-06 16:00:58 -0500

    use a external dates

--- 04-05-2026/fronted/src/pages/EstudiantesPage.jsx
+++ 04-05-2026/fronted/src/pages/EstudiantesPage.jsx
@@ -1,10 +1,17 @@
 import Estudiante from "../components/Estudiante";
+import { listaEstudiantes } from "../utils/data";
 const EstudiantesPage = () => {
     return (
         <div>
+            {/*
             <Estudiante nombre={"Anaela Pozo"} edad={21} url={"https://www.google.com"} />
             <Estudiante nombre={"Daniel Carrion"} edad={23} url={"https://www.google.com"} />
-            <Estudiante nombre={"OwO"} edad={50} url={"https://www.google.com"} />
+            <Estudiante nombre={"OwO"} edad={50} url={"https://www.google.com"} />*/}
+            {
+                listaEstudiantes.map((estudiante) => {
+                    return <Estudiante nombre={estudiante.nombre} edad={estudiante.edad} url={estudiante.url}/>
+                })
+            }
         </div>
     )
 }
--- 04-05-2026/fronted/src/utils/data.js
+++ 04-05-2026/fronted/src/utils/data.js
@@ -3,4 +3,6 @@ export const listaEstudiantes = [
     { id: 2, nombre: "Daniel Carrion", edad: 23, url: "https://www.google.com" },
     { id: 3, nombre: "OwO", edad: 24, url: "https://www.google.com" },
     { id: 4, nombre: "XD", edad: 25, url: "https://www.google.com" },
+    { id: 5, nombre: "UwU", edad: 28, url: "https://www.google.com" },
+    { id: 6, nombre: "dddd", edad: 25, url: "https://www.google.com" }
 ]
