22676cd8d4a0b8cefa321912e6a40dd8c81da5bb
SailorJupiterUwU <anaelapozo192@gmail.com>
2026-05-13 16:01:40 -0500

    postman y json-server

--- 04-05-2026/fronted/src/pages/EstudiantesPage.jsx
+++ 04-05-2026/fronted/src/pages/EstudiantesPage.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Estudiante from "../components/Estudiante";
-import { listaEstudiantes } from "../utils/data";
+//import { listaEstudiantes } from "../utils/data"; porque se usa otro servidor como fuente de verdad
 import EstudianteForm from "../components/EstudianteForm";
+//biblioteca para acciones http
+import axios from 'axios';
+
 
 const EstudiantesPage = () => {
     //Variable de estado para la lista de estudiantes
     
     //1) Paso Estado Global [Lifting State UP]
-    const [lstEstudiantes, setLstEstudiantes] = useState(listaEstudiantes);
+    const [lstEstudiantes, setLstEstudiantes] = useState([]);
+    
+    useEffect(()=> {
+        /*1. peticion http usando axios*/
+        axios.get("http://172.31.45.10:8000/estudiantes")//Puede retornar fallido o exitoso, se gestiona con promesas
+        /*2. manejo de una promesa (pendiente, resuelto o fallido son estados de una petición, por lo que se usa promesa)*/
+            .then((res) => {
+                console.log(res);
+                setLstEstudiantes(res.data)
+            }) //se ejecuta cuando es exitosa, cuando es exitoso es 200 de codigo de html
+        .catch((err)=> console.log(err)) //se ejecuta cuando es fallido, codigo diferente de 200
+        /*3. manejo de side-effects utilizando un hook useEffect (evita el rendeirizado infinito, se asegura que solo se ejecute una vez )*/ 
+    } , [])//el vector de dependencIA VACIO PARA QUE SE EJECUTE UNA VEZ
+    
     
     console.log("Renderizando...");
        /*const handleAgregarEstudiante = (e) => {
--- 04-05-2026/fronted/src/utils/data.js
+++ 04-05-2026/fronted/src/utils/data.js
@@ -1,8 +1,8 @@
-export const listaEstudiantes = [
+/*export const listaEstudiantes = [
     { id: 1, nombre: "Anaela Pozo", edad: 21, url: "https://www.google.com" },
     { id: 2, nombre: "Daniel Carrion", edad: 23, url: "https://www.google.com" },
     { id: 3, nombre: "OwO", edad: 24, url: "https://www.google.com" },
     { id: 4, nombre: "XD", edad: 25, url: "https://www.google.com" },
     { id: 5, nombre: "UwU", edad: 28, url: "https://www.google.com" },
     { id: 6, nombre: "dddd", edad: 25, url: "https://www.google.com" }
-]
+]*/
